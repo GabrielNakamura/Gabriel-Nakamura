4b9b67f7a15de4430da6b8b2796088e750964f3e
SeuNomeDeUsuario <seuMail@example.br>
2021-06-24 09:48:18 -0300

    updating widgets

--- commands_Hugo.R
+++ commands_Hugo.R
@@ -9,8 +9,12 @@ blogdown::new_post(title = "Hi Hugo",
                    ext = '.Rmarkdown', 
                    subdir = "post")
 serve_site()
-
-
+file.edit(".gitignore")
+blogdown::check_gitignore()
 rstudioapi::navigateToFile("config.yaml", line = 3)
 blogdown::config_netlify()
 blogdown::check_netlify()
+blogdown::check_hugo()
+rstudioapi::navigateToFile("config/_default/params.yaml")
+rstudioapi::navigateToFile("config/_default/params.yaml")
+rstudioapi::navigateToFile("config/_default/menus.yaml")
